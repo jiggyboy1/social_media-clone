4a0457ef072db10a97cbb08981f42136498eb0a6
kelechi chukwu <leochukwu48@gmail.com>
2024-07-18 08:11:14 -0700

    initial commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500,blank=True)
+    profile_picture = models.ImageField(upload_to="profile_picture/",blank=True)
+
+    def __str__(self) -> str:
+        return f"Profile {self.id}"
+
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+post_save.connect(create_profile,sender=User)
+    
+
+class Post(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/')
+    caption = models.TextField(blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Post From {self.user.username}'
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    text =  models.TextField(max_length=400)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Comment {self.text[0:30]}'
