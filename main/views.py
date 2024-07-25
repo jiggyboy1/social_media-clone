fa830d1edac3f998f5b49d803fe9a2205d6d194d
kelechi chukwu <leochukwu48@gmail.com>
2024-07-25 01:38:40 -0700

    initial commit

--- main/views.py
+++ main/views.py
@@ -16,3 +16,19 @@ def post_detail(request,post_id):
     context = {"post":post}
     return render(request,'post.html',context)
 
+def like_detail(request,like_post):
+    user = request.user
+    post = Post.objects.get(id=like_post)
+    current_like = post.likes
+    liked = Like.objects.filter(user=user,post=post).count()
+
+    if not liked:
+        liked = Like.objects.create(user=user,post=post)
+        current_like = current_like + 1
+    else:
+        liked = Like.objects.filter(user=user,post=post).delete()
+        current_like = current_like - 1
+
+    post.likes = current_like
+    post.save()
+    return HttpResponseRedirect(reverse('post_detail',args=[like_post]))
